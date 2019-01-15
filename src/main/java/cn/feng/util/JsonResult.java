fc9e8d435968c19c25ef4878619b602e2da66263
feng <hihchao@163.com>
2019-01-15 15:07:23 +0800

    Update JsonResult.java

--- src/main/java/cn/feng/util/JsonResult.java
+++ src/main/java/cn/feng/util/JsonResult.java
@@ -10,41 +10,46 @@ public class JsonResult {
 	
 	private String message;
 	private Boolean result;
-	private Object object;
+	private Object list;
 	private int total;
+	
+	public JsonResult(String message, Boolean result) {
+		this.message = message;
+		this.result = result;
+	}
 
+	public JsonResult(String message, Boolean result,Object list) {
+		this.message = message;
+		this.result = result;
+		this.list = list;
+	}
 	/**
 	 * @param message		返回的信息
 	 * @param result		返回成功或失败
-	 * @param object
+	 * @param list
 	 * @param total		后台管理需要的集合总条数
 	 */
-	public JsonResult(String message, Boolean result, Object object, int total) {
-		super();
+	public JsonResult(String message, Boolean result, Object list, int total) {
 		this.message = message;
 		this.result = result;
-		this.object = object;
+		this.list = list;
 		this.total = total;
 	}
 
 	public static JsonResult buildSuccessJsonResult(String message){
-		return new JsonResult(message,true,null,0);
+		return new JsonResult(message,true);
 	}
 
-	public static JsonResult buildSuccessJsonResult(String message,Object object,int total){
-		return new JsonResult(message,true,object, total);
+	public static JsonResult buildSuccessJsonResult(String message, Object list){
+		return new JsonResult(message,true ,list);
 	}
 
-	public static JsonResult buildSuccessJsonResult(String message,Object object){
-		return new JsonResult(message,true,object,0);
+	public static JsonResult buildSuccessJsonResult(String message, Object list, int total){
+		return new JsonResult(message,true,list, total);
 	}
 
 	public static JsonResult buildFailedJsonResult(String message){
 		return new JsonResult(message,false,null,0);
-	}
-
-	public static JsonResult buildFailedJsonResult(String message,Object object){
-		return new JsonResult(message,false,object,0);
 	}
 
 }
